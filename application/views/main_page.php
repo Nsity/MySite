fee6bd10f0127902609072be31892df5354d59a7
fedorova <nsitynastya@gmail.com>
2017-04-25 13:12:41 +0300

    Подключение bootstrap 4

--- application/views/main_page.php
+++ application/views/main_page.php
@@ -1,13 +1,22 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
-</head>
-<body>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="<?php echo base_url(); ?>bootstrap/css/bootstrap.min.css">
+
+    <script src="<?php echo base_url(); ?>js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url(); ?>bootstrap/js/jquery-3.2.1.min.js"></script>
 
 
-</body>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    <button class="btn btn-primary">Hello</button>
+  </body>
 </html>
